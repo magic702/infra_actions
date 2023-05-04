4feebcc35460765c8fa664605de76d50324072f6
Максим Милованов <magic702@yandex.ru>
2023-05-04 21:12:17 +0300

    Second commit

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -3,11 +3,15 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
 SECRET_KEY = 'k9gwcfq01xszx2(_4e#gc3u$rt=)y6ivx=$wt+5&-^ktk4dg4e'
+
 
 DEBUG = True
 
+
 ALLOWED_HOSTS = []
+
 
 INSTALLED_APPS = [
     'infra_app.apps.InfraAppConfig',
@@ -19,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -29,7 +34,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
 ROOT_URLCONF = 'infra_project.urls'
+
 
 TEMPLATES = [
     {
@@ -47,7 +54,9 @@ TEMPLATES = [
     },
 ]
 
+
 WSGI_APPLICATION = 'infra_project.wsgi.application'
+
 
 DATABASES = {
     'default': {
@@ -55,6 +64,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -71,14 +81,20 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
 LANGUAGE_CODE = 'en-us'
+
 
 TIME_ZONE = 'UTC'
 
+
 USE_I18N = True
+
 
 USE_L10N = True
 
+
 USE_TZ = True
+
 
 STATIC_URL = '/static/'
